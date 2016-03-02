65e9b6d4e7a4ae80a16bdd009b70f75627befde3
ipsaous <ipsaous@hotmail.com>
2016-03-02 16:18:16 +0100

    Changement de la page SingleTuto

--- src/CoursdeGratteBundle/Controller/TutoController.php
+++ src/CoursdeGratteBundle/Controller/TutoController.php
@@ -4,6 +4,7 @@ namespace CoursdeGratteBundle\Controller;
 
 
 use CoursdeGratteBundle\Entity\Tutovideo;
+use CoursdeGratteBundle\Utility\MyUtility;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TutoController extends Controller{
@@ -16,8 +17,10 @@ class TutoController extends Controller{
         $id = (int) $id;
         $em = $this->getDoctrine()->getManager();
         $tuto = $em->getRepository("CoursdeGratteBundle:Tutovideo")->findTutoByIdSlug($id, $slug);
+        $description = "";
         if($tuto !== null){
             $videoLinks = $this->getVideoLink($tuto);
+            $description = MyUtility::getDescriptionYoutube($tuto->getLientuto());
         }else{
             throw new \Exception("Page Introuvable. Il Semblerait que ce tuto n'existe plus");
         }
@@ -25,8 +28,15 @@ class TutoController extends Controller{
         return $this->render("CoursdeGratteBundle:Tuto:index.html.twig",
             array(
                 'tuto' => $tuto,
-                'videoLinks' => $videoLinks
+                'videoLinks' => $videoLinks,
+                'description' => $description
             ));
+    }
+
+    public function suggestionsAction($profId){
+        $em = $this->getDoctrine()->getManager();
+        $tutos = $em->getRepository("CoursdeGratteBundle:Tutovideo")->findBy(array('idProf' => $profId), null, 5, 0);
+        return $this->render("partials/vignette.html.twig", array('tutos' => $tutos));
     }
 
     public function getVideoLink(Tutovideo $tuto){
--- src/CoursdeGratteBundle/Utility/MyUtility.php
+++ src/CoursdeGratteBundle/Utility/MyUtility.php
@@ -27,4 +27,30 @@ class MyUtility {
     	return true;
     }
 
+    public static function getDescriptionYoutube($idYoutube){
+        //récupération de la description via youtube
+        $JSON = file_get_contents("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=".$idYoutube."&key=AIzaSyDGo5E2hMOmCbgkRqFExpiN9LtcN5DDtkA");
+        $data = json_decode($JSON);
+        //var_dump($data);
+        $testItem = $data->items; // Pour vérifier que l'on a une description ( que l'id de la video existe quoi)
+        if(!empty($testItem)){
+            $data = $data ->items[0]->snippet->description;
+            $content = $data;
+
+            //Pattern to retrieve the url in the comment
+            $pattern = '`((?:https?|ftp)://\S+?)(?=[[:punct:]]?(?:\s|\Z)|\Z)`';
+            //Function to wrap long URLs into smaller one before replacement in the preg_replace_callback() function
+            //Fonction permettant de raccourcir les longues URL lors du remplacement dans la fonction preg_replace_callback()
+            $callback_function = create_function(
+                '$matches',
+                '$link_displayed = (strlen($matches[0]) > 50) ? substr( $matches[0], 0, 50).\'[&hellip;]\'.substr($matches[0], -10) : $matches[0];
+						 return \'<a href="\'.$matches[0].\'" title="\'.$matches[0].\'" class="website" target="_blank">\'.$link_displayed.\'</a>\';'
+            );
+            $content = preg_replace_callback($pattern, $callback_function, $content);
+        }else{
+            $content = "Apparemment la vidéo n'existe plus. Merci de me le signaler ;)";
+        }
+        return $content;
+    }
+
 } 
